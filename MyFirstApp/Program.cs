d6bc7bad916868c35e34eeaec2e9371936e6fc7c
ALeksandr Utkin <utkin.aleksandr123@gmail.com>
2023-02-06 22:43:39 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,11 @@
+﻿//This is a  comment to my code
+
+//rakendus kusib kasutajalt sisestada oma nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name");
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+Console.WriteLine($"Hello, {userName} !");
